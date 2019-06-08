af8f9462fb634b329ea5296e8a8031f2551a7ccd
BudacaB <budaca.b@gmail.com>
2019-06-08 19:11:35 +0300

    scaffolding

---
+++ MindGeek.ImageManipulation/ConsoleOutput/Program.cs
@@ -0,0 +1,15 @@
+﻿using MindGeek.ImageManipulation;
+using System;
+
+
+namespace ConsoleOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ImageManipulation.ReturnHello());
+        }
+    }
+}
---
+++ MindGeek.ImageManipulation/MindGeek.ImageManipulation/ImageManipulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+
+namespace MindGeek.ImageManipulation
+{
+    public class ImageManipulation
+    {
+        public static string ReturnHello()
+        {
+            return "Hello World from library";
+        }
+
+    }
+}
