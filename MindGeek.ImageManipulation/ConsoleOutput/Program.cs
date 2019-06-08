e131bde582f8ae60df3b7fcedbf22c427d839054
BudacaB <budaca.b@gmail.com>
2019-06-08 22:20:10 +0300

    fetch from url and write input to console

--- MindGeek.ImageManipulation/ConsoleOutput/Program.cs
+++ MindGeek.ImageManipulation/ConsoleOutput/Program.cs
@@ -1,15 +1,22 @@
 ﻿using MindGeek.ImageManipulation;
 using System;
-
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleOutput
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Show(string toShow)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(ImageManipulation.ReturnHello());
+            Console.WriteLine(toShow);
+        }
+
+        public static void Main()
+        {
+            var result = ImageManipulation.Fetch("https://mgtechtest.blob.core.windows.net/files/showcase.json");
+            Show(result.Result);
         }
     }
 }
--- MindGeek.ImageManipulation/MindGeek.ImageManipulation/ImageManipulation.cs
+++ MindGeek.ImageManipulation/MindGeek.ImageManipulation/ImageManipulation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 
@@ -8,9 +9,20 @@ namespace MindGeek.ImageManipulation
 {
     public class ImageManipulation
     {
-        public static string ReturnHello()
+        public static async Task<string> Fetch(string urlToUse)
         {
-            return "Hello World from library";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string responseBody = await client.GetStringAsync(urlToUse);
+                    return responseBody;
+                }
+                catch (HttpRequestException e)
+                {
+                    return e.Message;
+                }
+            }
         }
 
     }
